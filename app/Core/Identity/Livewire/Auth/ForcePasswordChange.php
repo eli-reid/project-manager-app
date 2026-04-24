b8d143b0a69f795831dcc230f479afdebbdf254e
Eli Reid <Elir@elireid.com>
2026-04-24 05:32:16 -0400

    fix(password-change): change log level to debug for password update process

--- app/Core/Identity/Livewire/Auth/ForcePasswordChange.php
+++ app/Core/Identity/Livewire/Auth/ForcePasswordChange.php
@@ -34,7 +34,7 @@ class ForcePasswordChange extends Component
     public function updatePassword(): void
     {
 
-        Log::warning('User ID '.Auth::id().' is updating their password through the forced password change flow.');
+        Log::debug('User ID '.Auth::id().' is updating their password through the forced password change flow.');
         $validated = $this->validate([
             'password' => $this->passwordRules(),
         ]);
