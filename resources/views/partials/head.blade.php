bde8eaa38afadaf58941efc72dce27c611247ba0
eli-reid <elir@elireid.com>
2026-05-17 13:00:56 -0400

    Refactor code structure for improved readability and maintainability

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -16,6 +16,7 @@
 
 <link rel="manifest" href="/manifest.json">
 <link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="icon" href="/logo.png" type="image/png">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <meta name="msapplication-TileImage" content="/icon-192.png" />
