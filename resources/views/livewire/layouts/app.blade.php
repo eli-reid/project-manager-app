a4ed48c647d9a03ab1ba63e7b7ba522436014bcb
eli-reid <elir@elireid.com>
2026-05-24 17:54:25 -0400

    feat: Remove legacy layout files and refactor sidebar component for improved structure and maintainability

--- resources/views/livewire/layouts/app.blade.php
+++ resources/views/livewire/layouts/app.blade.php
@@ -4,7 +4,7 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
-        <livewire:layout.app-sidebar />
+        <livewire:layout.sidebar />
 
         @isset($domainNavbar)
             <div class="sticky top-0 z-30 border-b border-zinc-200 bg-zinc-50 px-3 py-2 dark:border-zinc-700 dark:bg-zinc-900 lg:ms-64">
