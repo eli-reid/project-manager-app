1d382d2cc685aa2149c11ffb885a831e1244c2b3
Eli Reid <Elir@elireid.com>
2026-06-22 06:32:14 -0400

    feat: implement settings management with new Setting class and form field types

--- app/Core/Notification/config/settings.php
+++ app/Core/Notification/config/settings.php
@@ -1,5 +1,24 @@
 <?php
 
-use App\Core\Notification\Settings\NotificationSettings;
+use App\Core\Settings\DTO\SettingFormFieldType;
+use App\Core\Settings\DTO\SettingType;
+use App\Core\Settings\DTO\Setting;
 
-return NotificationSettings::class;
+
+return [
+
+   new Setting(
+        $value = 'true',
+        $display_name = 'Enable Notifications',
+        $description = 'Master switch for notification delivery.',
+        $type = 'select',
+        $group = self::GROUP,
+        $options = ['true' => 'Enabled', 'false' => 'Disabled'],
+        $order = 1,
+        $is_visible = true,
+        $is_public = false,
+        $is_required = true,
+        $encrypted = false,
+    ),
+
+];
--- app/Domains/Projects/Contracts/ProjectTabPanelInterface.php
+++ app/Domains/Projects/Contracts/ProjectTabPanelInterface.php
@@ -4,7 +4,7 @@ namespace App\Domains\Projects\Contracts;
 
 use App\Domains\Projects\Models\Project;
 
-interface ProjectTabPanel
+interface ProjectTabPanelInterface
 {
     /**
      * @param  array<string, array{modeParam:string,mode:string,detailParam:?string,detailId:string,isCreateMode:bool}>  $tabContext
