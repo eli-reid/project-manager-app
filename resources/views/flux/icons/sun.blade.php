279b7e1909edde73d80fe4e28f816fd29af0419d
Eli Reid <Elir@elireid.com>
2026-07-08 05:44:06 -0400

    feat: add new weather icons for improved UI representation

---
+++ resources/views/flux/icons/sun.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes->merge(['xmlns' => 'http://www.w3.org/2000/svg','viewBox' => '0 0 24 24','fill' => 'none','stroke' => 'currentColor','aria-hidden' => 'true']) }}>
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 3v2m0 14v2m9-9h-2M5 12H3m15.364-6.364-1.414 1.414M7.05 16.95l-1.414 1.414M18.364 18.364l-1.414-1.414M7.05 7.05L5.636 5.636M12 8a4 4 0 100 8 4 4 0 000-8z"/>
+</svg>
